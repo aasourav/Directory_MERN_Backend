dcad3ceab9ab3ff9c9b90da314108a6fffc9c45a
Ahsan Amin <ahsan.amin.eu181400095@gmail.com>
2022-11-24 00:39:49 +0600

    abc

--- index.js
+++ index.js
@@ -19,14 +19,14 @@ const connect = async ()=>{
 
 app.use(express.json())
 app.use(cors({origin:'https://directory-mern-frontend.vercel.app'}))
-app.use((req, res, next) => {
-    const allowedOrigins = ['https://directory-mern-frontend.vercel.app'];
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-      res.setHeader('Access-Control-Allow-Origin', origin);
-    }
-    return next();
-  })
+// app.use((req, res, next) => {
+//     const allowedOrigins = ['https://directory-mern-frontend.vercel.app'];
+//     const origin = req.headers.origin;
+//     if (allowedOrigins.includes(origin)) {
+//       res.setHeader('Access-Control-Allow-Origin', origin);
+//     }
+//     return next();
+//   })
 app.use('/', folderRoute)
 
 
